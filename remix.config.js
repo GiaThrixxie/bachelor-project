a34a0099408d3d314060a0ea1ad9069aac7a7bf9
Line <treanii@hotmail.dk>
2023-12-19 10:02:45 +0100

    config changes

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,30 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
-  serverDependenciesToBundle: [/^react-icons/]
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+  serverDependenciesToBundle: [/^react-icons/],
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildPath: "build/index.js",
+  browserNodeBuiltinsPolyfill: {
+    modules: {
+      fs: true,
+      os: true,
+      path: true,
+      url: true,
+      http: true,
+      https: true,
+      events: true,
+      zlib: true,
+      stream: true,
+      util: true, 
+      buffer: true,
+      crypto: true,
+      querystring: true,
+      child_process: true,
+      net: true,
+      tls: true,
+      assert: true,
+    } 
+  }
 };
